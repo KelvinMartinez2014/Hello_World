5ac348d1382d2b173ec0da1458d8ec951436cf26
Junitchi <jonathan.roman@upr.edu>
2019-03-17 22:17:20 -0400

    Index.php

--- GreenBooks/GreenBooks/index.php
+++ GreenBooks/GreenBooks/index.php
@@ -2,8 +2,13 @@
 <html>
 <head>
 	<title>Main Page</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="resources/css/nav_bar.css">
+	<link rel="stylesheet" type="text/css" href="resources/css/bootstrap.css">
 </head>
 <body>
+	<?php include 'resources\php\nav_bar.php';?>
 	<p>This is going to be the main page!</p>
 </body>
 </html>
