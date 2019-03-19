deb7c1f68863b1969063d78f96792724306462cf
Junitchi <47652587+Junitchi@users.noreply.github.com>
2019-03-19 18:14:13 -0400

    Added new class

--- GreenBooks/GreenBooks/index.php
+++ GreenBooks/GreenBooks/index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<title>Main Page</title>
 
 	<!-- CSS -->
@@ -10,111 +13,357 @@
 
 
 	<?php include 'resources\php\nav_bar.php';?>
+	<div class="container">
+	<!-- Section: Magazine v.3 -->
+	<section class="magazine-section my-5">
 
-	<div class="container" style="margin-top: 2%">
+		<!-- Section heading -->
+		<h2 class="h1-responsive font-weight-bold text-center my-5">Popular Books</h2>
+		<!-- Section description -->
+		<p class="text-center w-responsive mx-auto mb-5">Duis aute irure dolor in reprehenderit in voluptate velit
+			esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+		qui officia deserunt mollit id laborum.</p>
+
+		<!-- Grid row -->
 		<div class="row">
-			<div class="col-md-12">
 
-				<div id="mdb-lightbox-ui"></div>
+			<!-- Grid column -->
+			<div class="col-lg-4 col-md-12 mb-lg-0 mb-5">
 
-				<div class="mdb-lightbox no-margin">
+				<!-- Featured news -->
+				<div class="single-news mb-3">
 
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(117).jpg"
-							class="img-fluid">
+					<!-- Image -->
+					<div class="view overlay rounded z-depth-2 mb-4">
+						<img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/86.jpg" alt="Sample image">
+						<a>
+							<div class="mask rgba-white-slight"></div>
 						</a>
-					</figure>
+					</div>
 
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(98).jpg"
-							class="img-fluid" />
-						</a>
-					</figure>
+					<!-- Grid row -->
+					<div class="row mb-3">
 
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(131).jpg"
-							class="img-fluid" />
-						</a>
-					</figure>
+						<!-- Grid column -->
+						<div class="col-12">
 
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(123).jpg"
-							class="img-fluid" />
-						</a>
-					</figure>
+							<!-- Badge -->
+							<a href="#!"><span class="badge pink"><i class="fas fa-camera pr-2" aria-hidden="true"></i>Ciencias</span></a>
 
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(118).jpg"
-							class="img-fluid" />
-						</a>
-					</figure>
+						</div>
+						<!-- Grid column -->
 
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(128).jpg"
-							class="img-fluid" />
-						</a>
-					</figure>
+					</div>
+					<!-- Grid row -->
 
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(132).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(132).jpg"
-							class="img-fluid" />
-						</a>
-					</figure>
-
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(115).jpg"
-							class="img-fluid" />
-						</a>
-					</figure>
-
-					<figure class="col-md-4">
-						<a href="https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg" data-size="1600x1067">
-							<img alt="picture" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(133).jpg"
-							class="img-fluid" />
-						</a>
-					</figure>
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a >Matematicas</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
 
 				</div>
+				<!-- Featured news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>Biologia</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>Quimica</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>Fisica</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0">
+							<a>Enfermeria</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
 
 			</div>
+			<!-- Grid column -->
+
+			<!-- Grid column -->
+			<div class="col-lg-4 col-md-6 mb-md-0 mb-5">
+
+				<!-- Featured news -->
+				<div class="single-news mb-3">
+
+					<!-- Image -->
+					<div class="view overlay rounded z-depth-2 mb-4">
+						<img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/31.jpg" alt="Sample image">
+						<a>
+							<div class="mask rgba-white-slight"></div>
+						</a>
+					</div>
+
+					<!-- Grid row -->
+					<div class="row mb-3">
+
+						<!-- Grid column -->
+						<div class="col-12">
+
+							<!-- Badge -->
+							<a href="#!"><span class="badge deep-orange"><i class="fas fa-plane pr-2" aria-hidden="true"></i>Artes</span></a>
+
+						</div>
+						<!-- Grid column -->
+
+					</div>
+					<!-- Grid row -->
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a class="font-weight-bold">This is title of the news</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Featured news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>Trends in the blogosphere for 2016.</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>Where you eat the best lunch in Warsaw?</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>What camera take for holidays?</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0">
+							<a>Why you should visit Lisbon?</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+			</div>
+			<!-- Grid column -->
+
+			<!-- Grid column -->
+			<div class="col-lg-4 col-md-6 mb-0">
+
+				<!-- Featured news -->
+				<div class="single-news mb-3">
+
+					<!-- Image -->
+					<div class="view overlay rounded z-depth-2 mb-4">
+						<img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/52.jpg" alt="Sample image">
+						<a>
+							<div class="mask rgba-white-slight"></div>
+						</a>
+					</div>
+
+					<!-- Grid row -->
+					<div class="row mb-3">
+
+						<!-- Grid column -->
+						<div class="col-12">
+
+							<!-- Badge -->
+							<a href="#!"><span class="badge success-color"><i class="fas fa-plane pr-2" aria-hidden="true"></i>Miscelanio</span></a>
+
+						</div>
+						<!-- Grid column -->
+
+					</div>
+					<!-- Grid row -->
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a class="font-weight-bold">This is title of the news</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Featured news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>How to recognize the footsteps of wild animals?</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>National Parks in Poland.</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news mb-3">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0 mb-3">
+							<a>Traveling without littering the planet.</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+				<!-- Small news -->
+				<div class="single-news">
+
+					<!-- Title -->
+					<div class="d-flex justify-content-between">
+						<div class="col-11 text-truncate pl-0">
+							<a>How to protect rainforests?</a>
+						</div>
+						<a><i class="fas fa-angle-double-right"></i></a>
+					</div>
+
+				</div>
+				<!-- Small news -->
+
+			</div>
+			<!-- Grid column -->
+
 		</div>
-		<div class="row" style="background-color:green">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus eget nisl vel porttitor. Suspendisse tincidunt tristique commodo. In lobortis eu dolor quis tincidunt. Vivamus non erat nec massa commodo ultrices. Vestibulum ullamcorper ut orci eget varius. Aliquam nec odio suscipit velit sollicitudin pulvinar condimentum id metus. Suspendisse molestie magna mi. Mauris feugiat enim est, egestas ornare ipsum vulputate eget. Etiam in pellentesque elit.
+		<!-- Grid row -->
 
-				Sed vehicula dignissim diam a imperdiet. Curabitur euismod ornare sodales. Etiam et neque sem. Vivamus convallis, elit et sodales elementum, nibh sapien volutpat dui, ac tincidunt nibh velit at lectus. Mauris a nisi in tellus accumsan suscipit a nec magna. Sed ac sem sed tortor pharetra suscipit. Nam efficitur mattis magna, vitae iaculis ante commodo ultrices. Vestibulum bibendum libero in erat porttitor dignissim. Praesent consectetur quis leo vel porta. Pellentesque eget ligula sapien. Nam vehicula risus sagittis, lobortis ligula non, luctus dui.
+	</section>
+	
+	<!-- Section: Magazine v.3 -->
+	
+	<div class="row" style="background-color:gray">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus eget nisl vel porttitor. Suspendisse tincidunt tristique commodo. In lobortis eu dolor quis tincidunt. Vivamus non erat nec massa commodo ultrices. Vestibulum ullamcorper ut orci eget varius. Aliquam nec odio suscipit velit sollicitudin pulvinar condimentum id metus. Suspendisse molestie magna mi. Mauris feugiat enim est, egestas ornare ipsum vulputate eget. Etiam in pellentesque elit.
 
-				In rhoncus et eros id ullamcorper. Phasellus ligula risus, aliquam sed sodales in, varius eu diam. Cras tempor massa id dui hendrerit, quis vulputate mi accumsan. Ut maximus et elit ac pretium. Vestibulum imperdiet at justo quis imperdiet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras non mollis elit, et imperdiet lorem. Pellentesque consequat posuere commodo. Nulla facilisi. Etiam vitae tempor augue, vitae ultricies purus. Cras euismod ullamcorper accumsan. Etiam erat magna, lacinia at ligula mattis, semper tristique metus. Praesent faucibus tempor justo, id varius nulla. Morbi risus nisi, venenatis non metus semper, ultrices hendrerit urna. Donec vitae nunc malesuada, sollicitudin nibh sit amet, condimentum augue.
+			Sed vehicula dignissim diam a imperdiet. Curabitur euismod ornare sodales. Etiam et neque sem. Vivamus convallis, elit et sodales elementum, nibh sapien volutpat dui, ac tincidunt nibh velit at lectus. Mauris a nisi in tellus accumsan suscipit a nec magna. Sed ac sem sed tortor pharetra suscipit. Nam efficitur mattis magna, vitae iaculis ante commodo ultrices. Vestibulum bibendum libero in erat porttitor dignissim. Praesent consectetur quis leo vel porta. Pellentesque eget ligula sapien. Nam vehicula risus sagittis, lobortis ligula non, luctus dui.
 
-				Nunc sit amet arcu risus. Aliquam erat volutpat. Sed finibus ex eros, eget tempus dui malesuada quis. Morbi ut ex vestibulum, tincidunt velit sit amet, faucibus tortor. Nullam malesuada condimentum massa id sodales. Nunc tempus arcu et nibh faucibus sollicitudin. Aliquam iaculis quis odio non elementum.
+			In rhoncus et eros id ullamcorper. Phasellus ligula risus, aliquam sed sodales in, varius eu diam. Cras tempor massa id dui hendrerit, quis vulputate mi accumsan. Ut maximus et elit ac pretium. Vestibulum imperdiet at justo quis imperdiet. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras non mollis elit, et imperdiet lorem. Pellentesque consequat posuere commodo. Nulla facilisi. Etiam vitae tempor augue, vitae ultricies purus. Cras euismod ullamcorper accumsan. Etiam erat magna, lacinia at ligula mattis, semper tristique metus. Praesent faucibus tempor justo, id varius nulla. Morbi risus nisi, venenatis non metus semper, ultrices hendrerit urna. Donec vitae nunc malesuada, sollicitudin nibh sit amet, condimentum augue.
 
-				Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eleifend tristique lectus, in varius arcu mattis aliquam. Praesent ac bibendum libero. Nam turpis velit, condimentum at mattis id, finibus sed odio. Aenean ut ipsum mattis, dapibus lacus eget, vulputate arcu. Phasellus ornare neque non pulvinar accumsan. Donec dapibus mauris libero, ut volutpat elit consectetur et. Sed quis pellentesque libero. Aenean ullamcorper ornare interdum. Duis euismod ultrices nisi eget gravida. Phasellus egestas diam vitae velit elementum dapibus.
+			Nunc sit amet arcu risus. Aliquam erat volutpat. Sed finibus ex eros, eget tempus dui malesuada quis. Morbi ut ex vestibulum, tincidunt velit sit amet, faucibus tortor. Nullam malesuada condimentum massa id sodales. Nunc tempus arcu et nibh faucibus sollicitudin. Aliquam iaculis quis odio non elementum.
 
-			Generated 5 paragraphs, 384 words, 2662 bytes of Lorem Ipsum</p>
-		</div>
-		<div style="float: right;">
-			
-		</div>
-		
-		
+			Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eleifend tristique lectus, in varius arcu mattis aliquam. Praesent ac bibendum libero. Nam turpis velit, condimentum at mattis id, finibus sed odio. Aenean ut ipsum mattis, dapibus lacus eget, vulputate arcu. Phasellus ornare neque non pulvinar accumsan. Donec dapibus mauris libero, ut volutpat elit consectetur et. Sed quis pellentesque libero. Aenean ullamcorper ornare interdum. Duis euismod ultrices nisi eget gravida. Phasellus egestas diam vitae velit elementum dapibus.
+
+		Generated 5 paragraphs, 384 words, 2662 bytes of Lorem Ipsum</p>
 	</div>
-	<!-- Bootstrap tooltips -->
-	<script type="text/javascript" src="js/popper.min.js"></script>
+	<div style="float: right;">
 
-	<!-- MDB core JavaScript -->
-	<script type="text/javascript" src="js/mdb.js"></script>
+	</div>
 
-	<script type="text/javascript">
-		$(function () {
-			$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-		});
-	</script>
+
+</div>
+<!-- JQuery -->
+<script type="text/javascript" src="resources/vendor/mdb/js/jquery-3.3.1.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="resources/vendor/mdb/js/bootstrap.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="resources/vendor/mdb/js/popper.min.js"></script>
+
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="resources/vendor/mdb/js/mdb.js"></script>
+
+<script type="text/javascript">
+	$(function () {
+		$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+	});
+</script>
 </body>
 </html>
