eaf372fe7c17e8f990662b73b54bbb588083a336
Junitchi <47652587+Junitchi@users.noreply.github.com>
2019-03-21 15:39:56 -0400

    Update

--- GreenBooks/GreenBooks/resources/php/nav_bar.php
+++ GreenBooks/GreenBooks/resources/php/nav_bar.php
@@ -1,7 +1,7 @@
-<link rel="stylesheet" href="resources/css/nav_bar.css">
+
 <link rel="stylesheet" type="text/css" href="resources/css/bootstrap.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  
+<link rel="stylesheet" href="resources/css/nav_bar.css">
 <!-- <link href="resources/css/mdb.min.css" rel="stylesheet"> -->
 </head>
 <body>
--- GreenBooks/GreenBooks/signup.php
+++ GreenBooks/GreenBooks/signup.php
@@ -4,21 +4,42 @@
 	<title></title>
 
 	<?php include 'resources\php\nav_bar.php';?>
-	<div class="container" style="margin-top: 4%">
+
+	<div class="container-fluid" style=" background-image: url('resources/images/signup.jpg'); background-repeat: no-repeat;
+  background-size: cover; height: 100vh ">
 		<div class="row">
 			<div class="col-sm-8">
-				<img src="resources/images/signup.jpg" class="img-fluid" alt="Responsive image">
+				<!-- <img src="resources/images/signup.jpg" class="img-fluid" alt="Responsive image"> -->
 			</div>
-			<div class="col-sm-2">
+			<div class="col-sm-4" style="position: relative">
 				<!-- Material input -->
-				<div class="md-form">
-					<input type="text" id="form1" class="form-control">
-					<label for="form1">Example label</label>
+				<div class="adddivwithopacity formbackground" style="position: absolute" ></div>
+				<div class="formsignup">
+					<form style="margin-top: 70px">
+						<div class="form-group ">
+							<label class="font-weight-light" for="formGroupExampleInput">Username</label>
+							<input type="text" class="form-control transparentinput takeourborder insertbottomline insertleftline" id="formGroupExampleInput" placeholder="Username">
+						</div>
+						<div class="form-group ">
+							<label class="font-weight-light" for="formGroupExampleInput2">Email</label>
+							<input type="text" class="form-control transparentinput takeourborder insertbottomline insertleftline" id="formGroupExampleInput2" placeholder="Email">
+						</div>
+						<div class="form-group ">
+							<label class="font-weight-light" for="formGroupExampleInput2">Password</label>
+							<input type="text" class="form-control transparentinput takeourborder insertbottomline insertleftline" id="formGroupExampleInput2" placeholder="Password">
+						</div>
+						<div class="form-group ">
+							<label class="font-weight-light" for="formGroupExampleInput2">Verify Password</label>
+							<input type="text" class="form-control transparentinput takeourborder insertbottomline insertleftline" id="formGroupExampleInput2" placeholder="Password">
+						</div>
+					</form>
 				</div>
+				
 			</div>
+			
 		</div>
 	</div>
-		<script type="text/javascript" src="resources/vendor/mdb/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="resources/vendor/mdb/js/jquery-3.3.1.min.js"></script>
 	<!-- Bootstrap core JavaScript -->
 	<script type="text/javascript" src="resources/vendor/mdb/js/bootstrap.min.js"></script>
 	<!-- Bootstrap tooltips -->
