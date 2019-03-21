11953b0959a80bdc030ac564bea1811d9bc27e8d
Junitchi <47652587+Junitchi@users.noreply.github.com>
2019-03-21 14:46:32 -0400

    added sign up image

--- GreenBooks/GreenBooks/signup.php
+++ GreenBooks/GreenBooks/signup.php
@@ -7,12 +7,30 @@
 	<div class="container" style="margin-top: 4%">
 		<div class="row">
 			<div class="col-sm-8">
-				<p>this is a p</p>
+				<img src="resources/images/signup.jpg" class="img-fluid" alt="Responsive image">
 			</div>
 			<div class="col-sm-2">
-				
+				<!-- Material input -->
+				<div class="md-form">
+					<input type="text" id="form1" class="form-control">
+					<label for="form1">Example label</label>
+				</div>
 			</div>
 		</div>
 	</div>
+		<script type="text/javascript" src="resources/vendor/mdb/js/jquery-3.3.1.min.js"></script>
+	<!-- Bootstrap core JavaScript -->
+	<script type="text/javascript" src="resources/vendor/mdb/js/bootstrap.min.js"></script>
+	<!-- Bootstrap tooltips -->
+	<script type="text/javascript" src="resources/vendor/mdb/js/popper.min.js"></script>
+
+	<!-- MDB core JavaScript -->
+	<!-- <script type="text/javascript" src="resources/vendor/mdb/js/mdb.js"></script> -->
+
+	<script type="text/javascript">
+		$(function () {
+			$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+		});
+	</script>
 </body>
 </html>
