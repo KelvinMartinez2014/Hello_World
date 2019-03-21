48ba052664a22d27c36c3f6da3565e433d87ab39
Junitchi <47652587+Junitchi@users.noreply.github.com>
2019-03-21 14:43:08 -0400

    Update signup.php

--- GreenBooks/GreenBooks/signup.php
+++ GreenBooks/GreenBooks/signup.php
@@ -2,8 +2,17 @@
 <html>
 <head>
 	<title></title>
-</head>
-<body>
 
+	<?php include 'resources\php\nav_bar.php';?>
+	<div class="container" style="margin-top: 4%">
+		<div class="row">
+			<div class="col-sm-8">
+				<p>this is a p</p>
+			</div>
+			<div class="col-sm-2">
+				
+			</div>
+		</div>
+	</div>
 </body>
 </html>
