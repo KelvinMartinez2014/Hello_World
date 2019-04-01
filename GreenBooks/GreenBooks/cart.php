6317c8495e1b30d0a3c705ff9a19638f2fad87d9
Junitchi <jonathan.roman@upr.edu>
2019-03-31 20:58:58 -0400

    update

--- GreenBooks/GreenBooks/cart.php
+++ GreenBooks/GreenBooks/cart.php
@@ -1,11 +1,44 @@
-<?php
-		echo "<div class='container' style='height:100vh; width:20vh; background-color: red;'>
-			<p>This is a paragraph</p>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Main Page</title>
 
-			</div>
+	<!-- CSS -->
+	<link rel="stylesheet" href="resources/css/nav_bar.css">
+	<link rel="stylesheet" type="text/css" href="resources/css/bootstrap.css">
+	
 
-		";
+	<!-- ATTENTION: The following php closes head and opens body -->
+	<?php include 'resources/php/nav_bar.php';?>  
+	<div class="container transparentContainer" style="margin-top: 4%;">
+		<div style="margin-top: 4%">
+				<h3>There is nothing on your shopping cart</h3>
+		</div>
+	</div>
+	<!-- JQuery -->
+	<script type="text/javascript" src="resources/vendor/mdb/js/jquery-3.3.1.min.js"></script>
+	<!-- Bootstrap core JavaScript -->
+	<script type="text/javascript" src="resources/vendor/mdb/js/bootstrap.min.js"></script>
+	<!-- Bootstrap tooltips -->
+	<script type="text/javascript" src="resources/vendor/mdb/js/popper.min.js"></script>
 
+	<!-- MDB core JavaScript -->
+	<!-- <script type="text/javascript" src="resources/vendor/mdb/js/mdb.js"></script> -->
+	
+	<script type="text/javascript">
+		$(function () {
+			$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+		});
 
+		$(document).ready(function() {  
+			var body = document.body;
 
-?>
+			body.classList.add("mainmenubackground");
+		});
+
+	</script>
+</body>
+</html>
