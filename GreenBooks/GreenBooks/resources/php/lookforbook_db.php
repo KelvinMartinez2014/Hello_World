74e208e8a2badb43af41f53fc15d186df33bc91f
Junitchi <jonathan.roman@upr.edu>
2019-04-14 18:53:58 -0400

    Update lookforbook_db.php

--- GreenBooks/GreenBooks/resources/php/lookforbook_db.php
+++ GreenBooks/GreenBooks/resources/php/lookforbook_db.php
@@ -5,36 +5,55 @@
 	include 'config.php';
 	echo "Connected successfully";
 
+	$edited = false;
+
 	$sql = "SELECT * FROM books WHERE ";
 
-	if(isset($_POST['form-isbn']) && $_POST['form-isbn'] != " "){
-		$sql = $sql . "ISBN = \"".$_POST['form-isbn']."\"";
+	if(isset($_POST['form-isbn']) && $_POST['form-isbn'] != ""){
+		$sql = $sql . "ISBN LIKE \"%".$_POST['form-isbn']."%\"";
+		$edited = true;
 	}
-	// if(isset($_POST["form-name"]) && $_POST["form-name"] != " "){
-	// 	$sql = $sql . "book_name = '".$_POST["form-name"]."', ";
-	// }if(isset($_POST["form-author"]) && $_POST["form-author"] != " "){
-	// 	$sql = $sql . "book_author = '".$_POST["form-author"]."', ";
-	// // }
+	if(isset($_POST["form-name"]) && $_POST["form-name"] != ""){
+		if ($edited) {
+			$sql = $sql . " AND ";
+		}
+		$sql = $sql . "book_name = '".$_POST["form-name"]."'";
+		$edited = true;
+	}if(isset($_POST["form-author"]) && $_POST["form-author"] != ""){
+		if ($edited) {
+			$sql = $sql . " AND ";
+		}
+		$sql = $sql . "book_author LIKE '%".$_POST["form-author"]."%'";
+		$edited = true;
+	 }
 
-	// echo $sql;
+	//echo $sql;
 
-	// if(isset($_POST['form-date'])){
-	// 	$sql = $sql . "release_date = ".$_POST['form-date']." ";
-	// }
+	if(isset($_POST['form-date'])){
+		if ($edited) {
+			$sql = $sql . " AND ";
+		}
+		$sql = $sql . "release_date LIKE '%".$_POST['form-date']."%' ";
+		$edited = true;
+	}
 
-	// if(isset($_POST['form-edition'])){
-	// 	$sql = $sql . "edition = ".$_POST['form-edition']." ";
-	// }
-	echo $sql;
+	if(isset($_POST['form-edition'])){
+		if ($edited) {
+			$sql = $sql . " AND ";
+		}
+		$sql = $sql . "edition = ".$_POST['form-edition']." ";
+		$edited = true;
+	}
+	echo $sql . "<br>";
 	$result = mysqli_query($conn, $sql);
-	 echo("Error description: " . mysqli_error($conn));
+	 // echo("Error description: " . mysqli_error($conn));
 	$book_object = array();
 	while ($row = mysqli_fetch_assoc($result)) {
 		array_push($book_object, $row);
 	} 
 
-	for ($i=0; $i < sizeof($book_object)-1; $i++) { 
-		$book_object[i];
+	foreach ($book_object as $row){
+		echo $row["book_name"] . "<br>";
 	}
 
 
