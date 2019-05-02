ebf3cdc2a6969c335d56e7a89b5bcc07cdc50fe0
KelvinMartinez2014 <47366012+KelvinMartinez2014@users.noreply.github.com>
2019-05-02 04:30:57 -0400

    Update AddImage_db.php

--- GreenBooks/GreenBooks/AddImage_db.php
+++ GreenBooks/GreenBooks/AddImage_db.php
@@ -1,16 +1,18 @@
 <?php 
 	if(isset($_POST['save_btn']))
-			{
-				if($con=mysqli_connect('localhost','root','qwer1234','greenbooks'))
-				{
-					$filetemp= $_FILES ['img']['tmp_name'];
-					$filename=$_FILES['img']['name'];
-					$filetype= $_FILES['img']['type'];
-					$filepath= "resources/images/".$filename;
-					move_uploaded_file($filetemp,$filepath);
-					echo "Image Inserted Successfully...";
-					echo "Name = ".$filename." Path = ".$filepath." Temp-name = ".$filetemp;
-					header("location:login.php?signup=true");
-				}
-			}
+	{
+		if($con=mysqli_connect('localhost','root','qwer1234','greenbooks'))
+		{
+			$filetemp= $_FILES ['img']['tmp_name'];
+			$filename=$_FILES['img']['name'];
+			$filetype= $_FILES['img']['type'];
+			$filepath= "resources/images/".$filename;
+			$sql = "update users set Image=".$filepath." where UserID=14;";
+			$conn->query($sql);
+			move_uploaded_file($filetemp,$filepath);
+			echo "Image Inserted Successfully...";
+			echo "Name = ".$filename." Path = ".$filepath." Temp-name = ".$filetemp;
+			header("location:login.php?signup=true");
+		}
+	}
 ?>
