9d9aef125de867538ae959a204826b13d465bab9
KelvinMartinez2014 <47366012+KelvinMartinez2014@users.noreply.github.com>
2019-05-05 09:24:48 -0400

    Update AddImage_db.php

--- GreenBooks/GreenBooks/AddImage_db.php
+++ GreenBooks/GreenBooks/AddImage_db.php
@@ -1,18 +1,34 @@
 <?php 
+	session_start();
+
+	include 'resources/php/config.php';
+	
 	if(isset($_POST['save_btn']))
 	{
-		if($con=mysqli_connect('localhost','root','qwer1234','greenbooks'))
-		{
-			$filetemp= $_FILES ['img']['tmp_name'];
-			$filename=$_FILES['img']['name'];
-			$filetype= $_FILES['img']['type'];
-			$filepath= "resources/images/".$filename;
-			$sql = "update users set Image=".$filepath." where UserID=14;";
+		$filetemp= $_FILES ['img']['tmp_name'];
+		$filename=$_FILES['img']['name'];
+		$filetype= $_FILES['img']['type'];
+		$filepath= "resources/images/".$filename;
+		
+		if($filetype<>"image/jpeg" && $filetype<>"image/png"){
+			header("location:AddImage.php?error=Type");}
+		
+		$Images=array();
+		echo "Image Inserted Successfully...";
+		echo "Name = ".$filename." Path = ".$filepath." Temp-name = ".$filetemp." Filetype = ".$filetype;
+		$sql="select * from users order by UserID desc limit 1;";
+		$result=$conn->query($sql);
+		while ($row = mysqli_fetch_assoc($result)) {
+			array_push($Images, $row);
+		}
+		
+		foreach($Images as $Image){
+			$sql = "update users set Image=\"".$filepath."\" where UserID=".$Image['UserID'].";";
+			echo "<br>ImageID = ".$Image['UserID'];
+			echo "<br>ImagePath = ".$filepath;
 			$conn->query($sql);
 			move_uploaded_file($filetemp,$filepath);
-			echo "Image Inserted Successfully...";
-			echo "Name = ".$filename." Path = ".$filepath." Temp-name = ".$filetemp;
-			header("location:login.php?signup=true");
-		}
+		}	
+		header("location:login.php?signup=true");
 	}
 ?>
