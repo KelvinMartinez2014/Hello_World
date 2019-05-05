5f848fa53970373b5c8f34f29c521af1acbb92a3
KelvinMartinez2014 <47366012+KelvinMartinez2014@users.noreply.github.com>
2019-05-05 09:43:31 -0400

    Update cart_checkout.php

--- GreenBooks/GreenBooks/resources/php/cart_checkout.php
+++ GreenBooks/GreenBooks/resources/php/cart_checkout.php
@@ -6,6 +6,11 @@
 	echo "Connected successfully";
 	$cart_object = $_SESSION["cart"];
 	foreach($cart_object as $book){
+		
+		$sql= "INSERT INTO `Historial` (vendor_id, ISBN, book_name, book_author, release_date, edition, category, price) VALUES
+		('".$book['vendor_id']."','".$book['ISBN']."','".$book['book_name']."','".$book['book_author']."','".$book['release_date']."','".$book['edition']."','".$book['category']."','".$book['price']."')";
+		$result = mysqli_query($conn, $sql);
+		
 		$sql = "DELETE FROM `books` WHERE book_id = '" .$book["book_id"]."'";
 		$result = mysqli_query($conn, $sql);
 
